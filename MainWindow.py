e2b54832e5a35ffa65bff8e0368ff1e653d91b5d
Harsh Patel <harshaudi27@gmail.com>
2019-05-23 05:33:33 +0530

    Added basic GUI and corresponding backend

---
+++ MainWindow.py
@@ -0,0 +1,152 @@
+import sys
+import os
+
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QPushButton, QTextEdit, QListWidget, QProgressBar, QLabel
+
+path = os.path.dirname(__file__)
+qtCreatorFile = "MainWindow.ui" 
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(path + qtCreatorFile)
+
+class Software(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        '''
+        Initializing software
+        '''
+        super(Software, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.title = "Hyperspectral Unmixing Toolbox"
+
+        self.initUI()
+
+    def initUI(self):
+        '''
+        Initializing UI components and their respective listeners
+        '''
+        self.setWindowTitle(self.title)
+
+        # Input Label
+        self.input_label = QLabel("Input", self)
+        self.input_label.move(20, 30)
+
+        # Input browse button
+        self.input_browse = QPushButton("Browse", self)
+        self.input_browse.move(550,30)
+        self.input_browse.clicked.connect(self.on_click_input)
+
+        # Input text field
+        self.input_text = QTextEdit(self)
+        self.input_text.setGeometry(142,35,402,21)
+
+        # Output Label
+        self.output_label = QLabel("Output", self)
+        self.output_label.move(20, 91)
+
+        # Output browse button
+        self.output_browse = QPushButton("Browse", self)
+        self.output_browse.move(550,91)
+        self.output_browse.clicked.connect(self.on_click_output)
+
+        # Output text field
+        self.output_text = QTextEdit(self)
+        self.output_text.setGeometry(142,95,402,21)
+
+        # No of components Label
+        self.components_label = QLabel("Components", self)
+        self.components_label.move(20, 150)
+
+        # Components text field
+        self.components = QTextEdit(self)
+        self.components.setGeometry(142,155,502,21)
+
+        # Jobs Label
+        self.jobs_label = QLabel("No of jobs", self)
+        self.jobs_label.move(20, 210)
+
+        # Jobs text field
+        self.jobs = QTextEdit(self)
+        self.jobs.setGeometry(142,215,502,21)
+
+        # OK button
+        self.OK = QPushButton("OK", self)
+        self.OK.move(230, 250)
+        self.OK.clicked.connect(self.on_click_OK)
+
+        # Cancel button
+        self.cancel = QPushButton("Cancel", self)
+        self.cancel.move(380, 250)
+        self.cancel.clicked.connect(self.on_click_cancel)
+
+        # Logs entry
+        self.logs = QListWidget(self)
+        self.logs.setGeometry(10, 300, 640, 130)
+
+        # Progress Bar
+        self.progress = QProgressBar(self)
+        self.progress.setGeometry(10, 450, 640, 20)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_click_input(self):
+        '''
+        On click listener for input_browse button
+        '''
+        self.InputBrowse()
+
+    def InputBrowse(self):
+        '''
+        Opens Browse Files dialog box for selecting input dataset
+        '''
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Dataset", "","All Files (*);;Matlab Files (*.mat)", options=options)
+        if fileName:
+            self.input_text.setText(fileName)
+
+    @pyqtSlot()
+    def on_click_output(self):
+        '''
+        On click listener for output_browse button
+        '''
+        self.OutputBrowse()
+
+    def OutputBrowse(self):
+        '''
+        Opens Browse Files dialog box for selecting target file for writing output
+        '''
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Target File", "","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            self.output_text.setText(fileName)
+
+    @pyqtSlot()
+    def on_click_OK(self):
+        '''
+        On click listener for OK button
+        '''
+        pass
+
+    @pyqtSlot()
+    def on_click_cancel(self):
+        '''
+        On click listener for Cancel button
+        '''
+        self.input_text.setText("")
+        self.output_text.setText("")
+        self.components.setText("")
+        self.jobs.setText("")
+        self.progress.setValue(0)
+        self.logs.clear()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Software()
+    window.show()
+    sys.exit(app.exec_())
